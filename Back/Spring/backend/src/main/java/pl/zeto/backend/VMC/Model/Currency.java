5266ac7f1a0ff20d80ae70d659da6a2f8315a010
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-02-25 22:38:52 +0100

    Creating basic model for Currency

---
+++ Back/Spring/backend/src/main/java/pl/zeto/backend/VMC/Model/Currency.java
@@ -0,0 +1,33 @@
+package pl.zeto.backend.VMC.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Currency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code; // Kod waluty, np. "USD"
+    private String name; // Nazwa waluty, np. "dolar amerykański"
+    private Double exchangeRate; // Kurs wymiany względem waluty bazowej, np. PLN
+
+    // Konstruktory, gettery i settery
+    public Currency() {
+    }
+
+    public Currency(String code, String name, Double exchangeRate) {
+        this.code = code;
+        this.name = name;
+        this.exchangeRate = exchangeRate;
+    }
+
+}
