e6b736210bdf96602d35532a6d54fb13a6498af1
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-02-25 22:39:00 +0100

    Creating basic model for User

---
+++ Back/Spring/backend/src/main/java/pl/zeto/backend/VMC/Model/AppUser.java
@@ -0,0 +1,35 @@
+package pl.zeto.backend.VMC.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    // Konstruktor bezargumentowy potrzebny dla JPA
+    public AppUser() {
+    }
+
+    // Konstruktor z wszystkimi polami, może być użyteczny w logice biznesowej
+    public AppUser(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    // Gettery i Settery są generowane przez Lombok, ale możesz je również dodać ręcznie
+}
