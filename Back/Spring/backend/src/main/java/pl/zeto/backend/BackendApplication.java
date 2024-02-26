2e9f7e4fb89c6c717cab458a16e7ee0c90884947
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-02-26 19:00:16 +0100

    Config app

--- Back/Spring/backend/src/main/java/pl/zeto/backend/BackendApplication.java
+++ Back/Spring/backend/src/main/java/pl/zeto/backend/BackendApplication.java
@@ -2,18 +2,13 @@ package pl.zeto.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-
 public class BackendApplication {
 
-    @GetMapping("/message")
-    public String getMessage(){
-        return "test message";
-    }
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
