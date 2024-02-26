05b58b71f780db91fe6b6543cbdec422c2efbe1e
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-02-26 11:10:21 +0100

    Gotowy backend do pracy
    Wyłączyłem spring security w celu szybszej pracy mniejsze problemy z postmanem itd.

---
+++ Back/Spring/backend/src/main/java/pl/zeto/backend/VMC/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package pl.zeto.backend.VMC.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+// Wyłączenie logowania i autentykacji
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+        return http.build();
+    }
+}
--- Back/Spring/backend/src/main/java/pl/zeto/backend/VMC/repository/CurrencyRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.zeto.backend.VMC.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.zeto.backend.VMC.model.Currency;
-
-public interface CurrencyRepository extends JpaRepository<Currency, Long> {
-    Currency findByCode(String code);
-}
