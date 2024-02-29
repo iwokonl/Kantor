4337e786fa3ca439aed45ef004af075399bb6f77
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-03-01 00:09:04 +0100

    Change to Angular "no standalone"

--- Front/.idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,3 +0,0 @@
-
-J
-angular-Kantor/src/main.ts,e\7\e75c7ff1405523faa495c903d514b3071f8dd77f
--- Front/Angular-Kantor/src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import {HeaderComponent} from "../header/header.component";
-
-export const routes: Routes = [{ path: '', component: HeaderComponent  }];
