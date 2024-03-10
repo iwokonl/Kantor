05f5de38e3c5ddbcea7b57a82817dcb05971033f
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-03-10 21:55:08 +0100

    Adding insert of currencies

---
+++ Back/Spring/backend/src/main/java/pl/zeto/backend/VMC/repository/CurrencyRepo.java
@@ -0,0 +1,19 @@
+package pl.zeto.backend.VMC.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import pl.zeto.backend.VMC.model.Currency;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CurrencyRepo extends JpaRepository<Currency, Long> {
+
+    Optional<List<Currency>> findByCodeStartingWith(String code);
+    Optional<List<Currency>> findByNameStartingWith(String name);
+    Optional<Currency> findByName(String name);
+
+    Optional<Currency> findByCode(String Code);
+
+}
