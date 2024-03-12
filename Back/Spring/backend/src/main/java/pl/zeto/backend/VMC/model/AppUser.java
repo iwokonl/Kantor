02668e55f354c2717a3550809a2fdac4966a9424
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-03-12 18:07:30 +0100

    Adding userdetails to appuser

--- Back/Spring/backend/src/main/java/pl/zeto/backend/VMC/model/AppUser.java
+++ Back/Spring/backend/src/main/java/pl/zeto/backend/VMC/model/AppUser.java
@@ -2,7 +2,12 @@ package pl.zeto.backend.VMC.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -12,10 +17,11 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class AppUser {
+public class AppUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
     private Role role;
     @Column(unique = true)
     private String username;
@@ -28,6 +34,31 @@ public class AppUser {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<AppAccount> accounts;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
     // Usunięcie odniesienia do encji Role
     // Konstruktor, gettery, settery pozostają niezmienione
