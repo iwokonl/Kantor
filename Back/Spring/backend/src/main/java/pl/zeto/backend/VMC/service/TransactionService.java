0fd42dbb6749736c13f99cb1dfd7af07415e8b8a
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-03-22 08:51:55 +0100

    Using in all files AppExeption

--- Back/Spring/backend/src/main/java/pl/zeto/backend/VMC/service/TransactionService.java
+++ Back/Spring/backend/src/main/java/pl/zeto/backend/VMC/service/TransactionService.java
@@ -1,7 +1,9 @@
 package pl.zeto.backend.VMC.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import pl.zeto.backend.VMC.exeption.AppExeption;
 import pl.zeto.backend.VMC.model.ForeignCurrencyAccount;
 import pl.zeto.backend.VMC.model.Transaction;
 import pl.zeto.backend.VMC.repository.AccountRepo;
@@ -17,14 +19,14 @@ public class TransactionService {
 
     @Autowired
     private AccountRepo accountRepository;
-
+d
     public Transaction addTransaction(Transaction transaction, Long accountId) {
         Set<ForeignCurrencyAccount> accounts = transaction.getUser().getAccounts(); // Odniesienie do wielu kont
 
         ForeignCurrencyAccount account = accounts.stream()
                 .filter(acc -> acc.getId().equals(accountId))
                 .findFirst()
-                .orElseThrow(() -> new RuntimeException("Konto o ID " + accountId + " nie zostało znalezione."));
+                .orElseThrow(() -> new AppExeption("Konto o ID " + accountId + " nie zostało znalezione.", HttpStatus.NOT_FOUND));
 
         if(account != null) {
             // Tutaj możesz dodać logikę aktualizacji salda konta, jeśli jest to wymagane
@@ -33,13 +35,13 @@ public class TransactionService {
             accountRepository.save(account); // Zapisz aktualizacje konta
         } else {
             // Jeśli transakcja nie ma przypisanego konta, rzucamy wyjątek lub obsługujemy błąd
-            throw new RuntimeException("Transakcja nie jest przypisana do żadnego konta.");
+            throw new AppExeption("Transakcja nie jest przypisana do żadnego konta.", HttpStatus.BAD_REQUEST);
         }
 
         return transactionRepository.save(transaction);
     }
 
     public Transaction getTransaction(Long id) {
-        return transactionRepository.findById(id).orElseThrow(() -> new RuntimeException("Transakcja o ID " + id + " nie została znaleziona."));
+        return transactionRepository.findById(id).orElseThrow(() -> new AppExeption("Transakcja o ID " + id + " nie została znaleziona.", HttpStatus.NOT_FOUND));
     }
 }
