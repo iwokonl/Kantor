b29badc95fb98995b0ac299c0b1de6bd9f0ac87a
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-04-15 15:48:18 +0200

    Chaning getCurrencyAccounts to no payload from payloaded

--- Back/Spring/backend/src/main/java/pl/kantor/backend/MVC/controller/ForeignCurrencyAccountController.java
+++ Back/Spring/backend/src/main/java/pl/kantor/backend/MVC/controller/ForeignCurrencyAccountController.java
@@ -2,11 +2,14 @@ package pl.kantor.backend.MVC.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import pl.kantor.backend.MVC.dto.ForeignCurrencyAccountDto;
 import pl.kantor.backend.MVC.dto.UserIdDto;
 import pl.kantor.backend.MVC.model.ForeignCurrencyAccount;
 import pl.kantor.backend.MVC.service.ForeignCurrencyAccountService;
+import pl.kantor.backend.MVC.service.UserService;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,9 +20,14 @@ import java.util.Map;
 @RequestMapping("/ForeignCurrencyAccount")
 public class ForeignCurrencyAccountController {
     private final ForeignCurrencyAccountService foreignCurrencyAccountService;
+    private final UserService userService;
     @PostMapping("/getCurrencyAccounts")
-    public ResponseEntity<List<ForeignCurrencyAccountDto>> getCurrencyAccounts(@RequestBody UserIdDto payload) {
-        List<ForeignCurrencyAccountDto> accounts = foreignCurrencyAccountService.getAllAccountsByUserId(payload.userId());
+    public ResponseEntity<List<ForeignCurrencyAccountDto>> getCurrencyAccounts() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+        Map<String,String> response = userService.jwtInfo(currentUserName);
+        String userId = response.get("id");
+        List<ForeignCurrencyAccountDto> accounts = foreignCurrencyAccountService.getAllAccountsByUserId(Long.parseLong(userId));
         return ResponseEntity.ok(accounts);
     }
 }
