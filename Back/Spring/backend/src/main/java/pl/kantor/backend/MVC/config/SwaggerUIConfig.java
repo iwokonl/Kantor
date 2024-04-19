494a8e9d2e75676b6d0bb1c620ea907a5ffbc942
Iwo Stanisławski <iwo.stanislawski@icloud.com>
2024-04-20 00:35:05 +0200

    Create SwaggerUIConfig.java

---
+++ Back/Spring/backend/src/main/java/pl/kantor/backend/MVC/config/SwaggerUIConfig.java
@@ -0,0 +1,37 @@
+package pl.kantor.backend.MVC.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
+@Configuration
+public class SwaggerUIConfig {
+    @EventListener({ApplicationReadyEvent.class})
+    void applicationReadyEvent() {
+        browse("http://localhost:8082/api/swagger-ui/index.html#/");
+    }
+    public static void browse(String url) {
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
