46d3e7e836df516f8902747fdf24f10d64d2c165
Iwo Stanisławski <iwo.stanislawski@student.uwm.edu.pl>
2024-04-29 16:12:35 +0200

    Hotfix

--- Back/micro-services/gateway/src/main/java/org/example/gateway/config/SecurityConfig.java
+++ Back/micro-services/gateway/src/main/java/org/example/gateway/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                 .addFilterBefore(new JwtAuthFilter(userAuthProvider), BasicAuthenticationFilter.class) // Dodanie filtra autoryzacji
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Wyłączenie zarządzania sesją
                 .authorizeHttpRequests(request -> // Konfiguracja zabezpieczeń
-                        request.requestMatchers(HttpMethod.POST,  "api/v1/auth/login").permitAll() // Pozwala na wykonywanie zapytań POST na adresach: /login, /register
+                        request.requestMatchers(HttpMethod.POST,  "api/v1/auth/login", "/error").permitAll() // Pozwala na wykonywanie zapytań POST na adresach: /login, /register
                                 .requestMatchers(HttpMethod.GET).permitAll() // Pozwala na wykonywanie zapytań GET na adresach: /search/**
                                 .anyRequest().authenticated()); // Wymaga autoryzacji dla pozostałych zapytań
         return httpSecurity.build();
